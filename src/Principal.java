256a0073b73bedf552163b79e8d6848badf07c9b
Caroline Andrade <andradecostacaroline@gmail.com>
2023-08-11 21:14:39 -0300

    master: testando a CalculadoraDeTempo

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
@@ -6,7 +7,6 @@ public class Principal {
 
     public static void main(String[] args) {
 
-        // Titulo meuTitulo = new Filme();
         // parou de compilar porque os atributos da classe filme são privados
         // precisamos utilizar os métodos acessores
 //        meuFilme.nome = "The Matrix";
@@ -14,26 +14,41 @@ public class Principal {
 //        meuFilme.duracaoEmMinutos = 135;
 //        meuFilme.incluidoNoPlano = true;
 
-//        meuTitulo.setNome("The Matrix");
-//        meuTitulo.setAnoDeLancamento(1999);
-//        meuTitulo.setDuracaoEmMinutos(135);
-//
-//        meuTitulo.exibeFichaTecnica();
-//        meuTitulo.avalia(9);
-//        meuTitulo.avalia(8);
-//        meuTitulo.avalia(9);
-//
+
+        Titulo meuTitulo = new Filme();
+        meuTitulo.setNome("The Matrix");
+        meuTitulo.setAnoDeLancamento(1999);
+        meuTitulo.setDuracaoEmMinutos(135);
+
+        meuTitulo.exibeFichaTecnica();
+        meuTitulo.avalia(9);
+        meuTitulo.avalia(8);
+        meuTitulo.avalia(9);
+
 //        System.out.println("Média de avaliações do filme: " + meuTitulo.pegaMedia());
 //        System.out.println("Total de avaliações: " + meuTitulo.getTotalDeAvaliacoes());
 
+            // instanciando uma série
     Serie lost = new Serie();
     lost.setNome("Lost");
     lost.setAnoDeLancamento(2000);
     lost.setTemporadas(10);
     lost.setEpisodiosPorTemporada(10);
     lost.setMinutosPorEpisodio(50);
-
     System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+            // instanciando outro filme
+    Filme outroFilme = new Filme();
+    outroFilme.setNome("O poderoso chefão");
+    outroFilme.setAnoDeLancamento(1970);
+    outroFilme.setDuracaoEmMinutos(180);
+
+            // testando a Calculadora de Tempo
+    CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+    calculadora.inclui(lost);
+    calculadora.inclui(meuTitulo);
+    calculadora.inclui(outroFilme);
+    System.out.println(calculadora.getTempoTotal());
 
     }
 
--- src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculos/CalculadoraDeTempo.java
@@ -5,12 +5,23 @@ import br.com.alura.screenmatch.modelos.Titulo;
 public class CalculadoraDeTempo {
     private int tempoTotal;
 
+            // usa o pilimorfismo e escreve apenas um método
     public void inclui(Titulo titulo){
-        tempoTotal += titulo.getDuracaoEmMinutos();
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
 
     }
+//    public void inclui(Filme filme){
+//        this.tempoTotal += filme.getDuracaoEmMinutos();
+//
+//    }
+//
+//    public void inclui(Serie serie){
+//        this.tempoTotal += serie.getDuracaoEmMinutos();
+//
+//    }
 
     public int getTempoTotal() {
-        return tempoTotal;
+        return this.tempoTotal;
     }
 }
