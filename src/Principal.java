8a494021247ec0884cee33d0fe49b47f910b68a7
carolineandradecosta <andradecostacaroline@gmail.com>
2023-09-25 23:49:19 -0300

    master: testando o método toString

--- src/Principal.java
+++ src/Principal.java
@@ -80,6 +80,8 @@ public class Principal {
     listaDeFilmes.add(outroFilme);
     System.out.println("Tamanho da lista: " + listaDeFilmes.size());
     System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+    System.out.println(listaDeFilmes);
+    System.out.println("to string do filme " + listaDeFilmes.get(0).toString());
 
 
     }
