cf6e3264ee0cd203de313cb96a5d923248988d18
carolineandradecosta <andradecostacaroline@gmail.com>
2023-10-10 00:24:20 -0300

    master: criando o primeiro Request e Response de uma API pronta

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -0,0 +1,41 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca:");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=246c131b";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+                // guardando em uma variável o json que é o response.body();
+        String json = response.body();
+        System.out.println(json);
+
+                // criando uma instância da classe Gson
+                // essa classe foi baixada separadamente e adicionada ao projeto
+        Gson gson = new Gson();
+                // Desserialização: recebe um json e transforma em uma classe do tipo Titulo
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println("Título: " + meuTitulo.getNome());
+
+    }
+}
